8aa81330b3f06c26613f9dc65430fad589760ec1
wpioneer <wildpioneer@protonmail.ch>
2016-02-11 22:51:50 +0300

    Update Md wrapper

--- wrappers/md.jsx
+++ wrappers/md.jsx
@@ -7,7 +7,7 @@ import ReadNext from '../components/ReadNext';
 
 export default class extends React.Component {
   render() {
-    let post, home;
+    let post, home, postUrl, jsonLD;
     post = this.props.page.data;
     home = (
       <div>
@@ -20,22 +20,44 @@ export default class extends React.Component {
       </div>
     );
 
+    jsonLD = `
+      <script type="application/ld+json">
+        {
+            "@context": "http://schema.org",
+            "@type": "Article",
+            "publisher": "` + this.props.config.authorName + `",
+            "author": {
+                "@type": "Person",
+                "name": "` + this.props.config.authorName + `",
+                "url": "http://ashel.xyz/",
+                "sameAs": "http://ashel.xyz/"
+            },
+            "headline": "` + post.title + `",
+            "url": "",
+            "datePublished": "` + post.datePublished + `",
+            "dateModified": "` + post.dateModified + `",
+            "description": "` + post.description + `";
+        }
+      </script>
+    `;
+
     return (
       <DocumentTitle title={`${post.title} | ${this.props.config.blogTitle}`}>
         <div>
+          <div dangerouslySetInnerHTML={{__html: jsonLD}}/>
           {home}
           <div className='blog-single'>
             <div className='text'>
               <h1>{post.title}</h1>
               <div dangerouslySetInnerHTML={{__html: post.body}}/>
               <em>
-                Опубликовано {moment(post.date).lang('ru').format('D MMM YYYY')}
+                Опубликовано {moment(post.datePublished).locale('ru').format('D MMM YYYY')}
               </em>
             </div>
             <div className='footer'>
               <ReadNext post={post} {...this.props}/>
               <p>
-                <strong>{this.props.config.authorName}</strong> &copy; Все права сохранены. <a href="https://twitter.com/wpioneer0">@wpioneer on Twitter</a>
+                <strong>{this.props.config.authorName}</strong> &copy; Все права сохранены. <a href={this.props.config.twitter}>@wpioneer on Twitter</a>
               </p>
             </div>
           </div>
