2eefb1eae50a01d8927043991bb4e24357eafc0c
wpioneer <wildpioneer@protonmail.ch>
2016-02-18 23:37:16 +0300

    Update home template

--- pages/_template.jsx
+++ pages/_template.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { RouteHandler, Link } from 'react-router';
 import { link } from 'gatsby-helpers';
+import proxima from '../static/fonts/proxima/stylesheet.css';
 
 export default class extends React.Component {
   render() {
