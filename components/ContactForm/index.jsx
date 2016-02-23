42bc9889149ce357e860560005fbce43cd81014c
wpioneer <a.shelepenok@gmail.com>
2016-02-24 00:20:36 +0300

    Update

---
+++ components/ContactForm/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { RouteHandler, Link } from 'react-router'
+import { link } from 'gatsby-helpers'
+
+import './style.css'
+
+class ContactForm extends React.Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+
+export default ContactForm
