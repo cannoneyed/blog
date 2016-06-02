ecd32888467c3d19759dceb509c9568aa7139b83
wpioneer <a.shelepenok@gmail.com>
2016-06-02 21:58:20 +0300

    update

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,23 +1,18 @@
+var autoprefixer = require('autoprefixer');
 var rucksack = require('rucksack-css')
 var lost = require("lost")
 
 exports.modifyWebpackConfig = function(config, env) {
-    var is_static = env === 'static'
-    var is_develop = env === 'develop'
-    var is_production = env === 'production'
 
     config.merge({
         postcss: [
             lost(),
-            rucksack()
+            rucksack(),
+            autoprefixer({
+                browsers: ['>1%', 'ie >= 10', 'safari >= 6', 'last 3 versions']
+            })
         ]
-    });
-
-    config.loader('fonts', function(cfg) {
-        cfg.test = /\.(((woff|woff2|eot|ttf|svg)(\?[0-9]{8}))|(woff|woff2|eot|ttf|svg))$/
-        cfg.loader = 'file-loader'
-        return cfg
-    });
+    })
 
     return config
 };
