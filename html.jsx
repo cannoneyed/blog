80fd35753ed82e94a0db8596b333512730f3a291
wpioneer <a.shelepenok@gmail.com>
2016-06-07 03:48:00 +0300

    Added favicon

--- html.jsx
+++ html.jsx
@@ -12,22 +12,8 @@ module.exports = React.createClass({
     render() {
         const {body, route} = this.props
         const title = DocumentTitle.rewind()
-
         const description = ""
-
-        const font = `
-                          WebFontConfig = {
-                            google: { families: [ 'Roboto:400,500,700:latin,cyrillic' ] }
-                          };
-                          (function() {
-                            var wf = document.createElement('script');
-                            wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-                            wf.type = 'text/javascript';
-                            wf.async = 'true';
-                            var s = document.getElementsByTagName('script')[0];
-                            s.parentNode.insertBefore(wf, s);
-                          })();
-                      `
+        const font = <link href='https://fonts.googleapis.com/css?family=Roboto:400,400italic,500,700&subset=latin,cyrillic' rel='stylesheet' type='text/css' />
         let css
         if (process.env.NODE_ENV === 'production') {
             css = <style dangerouslySetInnerHTML={ {    __html: require('!raw!postcss!./public/styles.css')} } />
@@ -39,14 +25,15 @@ module.exports = React.createClass({
               <meta charSet="utf-8" />
               <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
               <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=5.0" />
+              <link rel='shortcut icon' type='image/x-icon' href='/favicon.ico?v=2' />
               <title>
                 { title }
               </title>
+              { font }
+              { css }
             </head>
             <body>
               <div id="react-mount" dangerouslySetInnerHTML={ {    __html: this.props.body} } />
-              <script dangerouslySetInnerHTML={ {    __html: font} } />
-              { css }
               <script src={ prefixLink(`/bundle.js?t=${BUILD_TIME}`) } />
             </body>
             </html>
